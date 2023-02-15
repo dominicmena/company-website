cce2339e8d31e0e586045c861f7975a829fa79e0
dominicmena <mena.dominic@gmail.com>
2023-02-15 17:30:22 -0600

    changed file name

--- server.js
+++ server.js
@@ -3,33 +3,35 @@ const express = require('express')
 const app = express()
 const PORT = 3000;
 
+app.use(express.static("public"));
+
 app.get('/', (request, response) => {
     response.send("Welcome to PhatDom Co.")
 })
 
-app.get('/home-page.ejs', (request,response) => {
-    response.send("Home page")
+app.get('/homepage', (request,response) => {
+    response.render("home-page.ejs")
 })
 
-app.get('/company-history.ejs', (request,response) => {
-    response.send("Company History")
-})
+// app.get('/company-history.ejs', (request,response) => {
+//     response.render("Company History")
+// })
 
-app.get('/staff-page.ejs', (request,response) => {
-    response.send("Staff Page")
-})
+// app.get('/staff-page.ejs', (request,response) => {
+//     response.render("Staff Page")
+// })
 
-app.get('/past-work.ejs', (request,response) => {
-    response.send("Past Work")
-})
+// app.get('/past-work.ejs', (request,response) => {
+//     response.render("Past Work")
+// })
 
-app.get('/contact-page.ejs', (request,response) => {
-    response.send("Contact Page")
-})
+// app.get('/contact-page.ejs', (request,response) => {
+//     response.render("Contact Page")
+// })
 
-app.get('/links-page.ejs', (request,response) => {
-    response.send("Links page")
-})
+// app.get('/links-page.ejs', (request,response) => {
+//     response.render("Links page")
+// })
 
 
 app.listen(PORT,()=>{
